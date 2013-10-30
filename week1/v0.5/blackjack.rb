4acb226dd453aabcfa55f91c25c9b72d8859b28a
NisarTahir <nisar.tahir@gmail.com>
2013-10-30 13:21:13 +0000

    .

--- week1/v0.5/blackjack.rb
+++ week1/v0.5/blackjack.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 def deal_card
 
-card_deck = { 'DA' => 11, 'D2' => 2, 'D3' => 3, 'D4' => 4 , 'D5' => 5 , 'D6' => 6 , 'D7' => 7 , 'D8' => 8, 'D9' => 9, 'D10' => 10, 'DJ' => 10, 'DQ' => 10, 'DK' => 10, 'HA' => 11, 'H2' => 2, 'H3' => 3, 'H4' => 4 , 'H5' => 5 , 'H6' => 6 , 'H7' => 7 , 'H8' => 8, 'H9' => 9, 'H10' => 10, 'HJ' => 10, 'HQ' => 10, 'HK' => 10,  'CA' => 11, 'C2' => 2, 'C3' => 3, 'C4' => 4 , 'C5' => 5 , 'C6' => 6 , 'C7' => 7 , 'C8' => 8, 'C9' => 9, 'C10' => 10, 'CJ' => 10, 'CQ' => 10, 'CK' => 10,  'SA' => 11, 'S2' => 2, 'S3' => 3, 'S4' => 4 , 'S5' => 5 , 'S6' => 6 , 'S7' => 7 , 'S8' => 8, 'S9' => 9, 'S10' => 10, 'SJ' => 10, 'SQ' => 10, 'SK' => 10 }
-#card_deck = { 'DA' => 11, 'D2' => 2, 'D3' => 3, 'HA' => 11 , 'CA' => 11 , 'SA' => 11, }
+#card_deck = { 'DA' => 11, 'D2' => 2, 'D3' => 3, 'D4' => 4 , 'D5' => 5 , 'D6' => 6 , 'D7' => 7 , 'D8' => 8, 'D9' => 9, 'D10' => 10, 'DJ' => 10, 'DQ' => 10, 'DK' => 10, 'HA' => 11, 'H2' => 2, 'H3' => 3, 'H4' => 4 , 'H5' => 5 , 'H6' => 6 , 'H7' => 7 , 'H8' => 8, 'H9' => 9, 'H10' => 10, 'HJ' => 10, 'HQ' => 10, 'HK' => 10,  'CA' => 11, 'C2' => 2, 'C3' => 3, 'C4' => 4 , 'C5' => 5 , 'C6' => 6 , 'C7' => 7 , 'C8' => 8, 'C9' => 9, 'C10' => 10, 'CJ' => 10, 'CQ' => 10, 'CK' => 10,  'SA' => 11, 'S2' => 2, 'S3' => 3, 'S4' => 4 , 'S5' => 5 , 'S6' => 6 , 'S7' => 7 , 'S8' => 8, 'S9' => 9, 'S10' => 10, 'SJ' => 10, 'SQ' => 10, 'SK' => 10 }
+card_deck = { 'DA' => 11, 'D2' => 2, 'D3' => 3, 'D4' => 4 , 'D5' => 5 , 'D6' => 6 , 'D7' => 7 , 'D8' => 8, 'HA' => 11 , 'CA' => 11 , 'SA' => 11, }
 
   #pull out a random card from all the cards from the deck  
 
@@ -41,23 +41,10 @@ def players_hand(players_cards)
       #count the number of Aces we find, starting with zero
       @eleven_found = @eleven_found + 1
     
-      if @eleven_found > 1
-        #When more than 1 Ace is found, only add 1
-        @player_card_number_total  =+ @player_card_number_total + 1
-
-      elsif @eleven_found == 1
-        #This is needed for the case when there is only 1 Ace
-        @player_card_number_total  =+ @player_card_number_total + y
-    
-      end
-
-    else
-
-        #This is needed for all other conditions but can only run when no Ace is found, otherwise if it 
-        #is not in it's own else clause, it would run again and add another 11!!!!!
-        @player_card_number_total  =+ @player_card_number_total + y
-
     end 
+
+    @player_card_number_total  =+ @player_card_number_total + y
+
 
     #Build a string of the current players position
     
@@ -79,23 +66,9 @@ def dealers_hand(dealers_cards)
       #count the number of Aces we find, starting with zero
       @eleven_found = @eleven_found + 1
     
-      if @eleven_found > 1
-        #When more than 1 Ace is found, only add 1
-        @dealer_card_number_total  =+ @dealer_card_number_total + 1
-
-      elsif @eleven_found == 1
-        #This is needed for the case when there is only 1 Ace
-        @dealer_card_number_total  =+ @dealer_card_number_total + y
+    end
     
-      end
-
-    else
-
-        #This is needed for all other conditions but can only run when no Ace is found, otherwise if it 
-        #is not in it's own else clause, it would run again and add another 11!!!!!
-        @dealer_card_number_total  =+ @dealer_card_number_total + y
-
-    end 
+    @dealer_card_number_total  =+ @dealer_card_number_total + y
 
     #Build a string of the current players position
     @current_dealer_cards = @current_dealer_cards  + card_grahic(x[1,2], x[0,1]) #" [" + y.to_s + " of " +  suit(x[0,1]) + "]"
@@ -208,10 +181,10 @@ catch(:stop) do
 
     players_hand(players_cards)
 
-    if @player_card_number_total >= 22 && @eleven_found == 1
+    if @player_card_number_total >= 22 && @eleven_found >= 1
 
-      @player_card_number_total = 21
-
+     @player_card_number_total = @player_card_number_total - (10 * @eleven_found)
+     
     end
     
     show_cards(@current_player_cards, @player_card_number_total)
@@ -222,6 +195,8 @@ catch(:stop) do
       puts "=========================GAME SUMMARY=============================="
       puts "Unfortunately, you busted the game #{username}"
       puts "=========================GAME SUMMARY==============================\n\n"
+      puts "Press Enter to continue ..."
+      user_response = gets.chomp
 
       break
 
@@ -258,6 +233,12 @@ catch(:stop) do
           #p "&&&&DEBUG INFO: (Dealer Cards that HAVE being dealt) #{dealers_cards}&&&&&"
 
           dealers_hand(dealers_cards)
+          
+          if @dealer_card_number_total >= 22 && @eleven_found >= 1
+
+            @dealer_card_number_total = @dealer_card_number_total - (10 * @eleven_found)
+
+          end
 
           show_cards(@current_dealer_cards, @dealer_card_number_total)
 
@@ -265,12 +246,6 @@ catch(:stop) do
 
           sleep 4
 
-          if @dealer_card_number_total >= 22 && @eleven_found == 1
-
-            @dealer_card_number_total = 21
-
-          end
-          
           if @dealer_card_number_total >= 17
             
             puts "=======================DEALER TURN ENDED===========================\n\n"
