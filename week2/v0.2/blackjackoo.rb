500de8261cc6761e4039b19d48afd67847eda8d3
NisarTahir <nisar.tahir@gmail.com>
2013-11-07 16:49:59 +0000

    .

--- week2/v0.2/blackjackoo.rb
+++ week2/v0.2/blackjackoo.rb
@@ -1,20 +1,5 @@
 require 'pry'
 
-class Card
-
-	attr_accessor :suit, :value
-
-	def initialize(n)
-
-	end
-
-	def card
-		@suit
-		@value
-	end
-
-end
-
 class Deck #class name must be Capitalised
 
 	attr_accessor :cards, :test
@@ -22,7 +7,7 @@ class Deck #class name must be Capitalised
 	def initialize(test)
 		@cards = []
 		@cards = ["HA", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "HJ", "HQ", "HK", "DA", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "DJ", "DQ", "DK", "SA", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "SJ", "SQ", "SK", "CA", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "CJ", "CQ", "CK"]
-#    @cards = ["HA", "DA", "CA", "SA", "H3", "H4", "H6", "HJ", "HQ", "HK" ]
+    #@cards = ["HA", "DA", "CA", "SA", "H3", "H4", "H6", "HJ", "HQ", "HK" ]
 		
 		shuffle_cards
 	end
